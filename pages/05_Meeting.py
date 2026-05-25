bd7b721667f196c454fa9b1e936d9102b38635c4
wavenexus-enterprice <wavenexus001@gmail.com>
2026-05-25 19:02:03 +0900

    #11 MEETINGGPT: 11.7 Refine Chain

--- pages/05_Meeting.py
+++ pages/05_Meeting.py
@@ -5,8 +5,18 @@ import subprocess
 import glob
 import os
 from openai import OpenAI
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.output_parsers import StrOutputParser
 
 has_transcript = os.path.exists("./.cache/files/podcast.txt")
+
+llm = ChatOpenAI(
+    model= "gpt-5-nano",
+    temperature= 0.1,
+)
 
 client = OpenAI()
 
@@ -94,3 +104,58 @@ if video:
     with transcript_tab:
         with open(transcript_path, "r") as file:
             st.write(file.read())
+
+    with summary_tab:
+        start = st.button("Generate summary")
+
+        if start:
+            loader = TextLoader(transcript_path)
+
+            splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                chunk_size=800,
+                chunk_overlap=100
+            )
+
+            docs = loader.load_and_split(text_splitter=splitter)
+            first_summary_prompt = ChatPromptTemplate.from_template(
+                """
+                    Write a concise summary of the following:
+                    "{text}"
+                    CONCISE SUMMARY:
+                """
+            )
+
+            first_summary_chain = first_summary_prompt | llm | StrOutputParser()
+
+            summary = first_summary_chain.invoke({
+                "text": docs[0].page_content
+            })
+
+            refine_prompt = ChatPromptTemplate.from_template(
+                """
+                Your job is to produce a final summary.
+                We have provided and existing summary up to a 
+                certain point: {existing_summary}
+                We have the opportunity to refine the existing
+                summary (only if needed) with some more context
+                below.
+                -----------
+                {context} 
+                -----------
+                Given the new context, refine the original
+                summary.
+                If the context isn't useful. RETURN the
+                original summary.
+                """
+            )
+
+            refine_chain = refine_prompt | llm | StrOutputParser()
+            
+            with st.status("Summarizing...") as status:
+                for i, doc  in enumerate(docs[1:]):
+                    status.update(label=f"Processing document {i+1}/{len(docs)-1}")
+                    summary = refine_chain.invoke({
+                        "existing_summary": summary,
+                        "context": doc.page_content
+                    })
+            st.write(summary)
